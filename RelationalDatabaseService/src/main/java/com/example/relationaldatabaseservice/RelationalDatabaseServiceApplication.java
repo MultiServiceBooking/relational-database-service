e1f774f716ad27d756edc4bc0e6ff85162434e8f
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2024-08-22 00:49:36 +0200

    fix: Fix initial project

---
+++ RelationalDatabaseService/src/main/java/com/example/relationaldatabaseservice/RelationalDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationaldatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalDatabaseServiceApplication.class, args);
+	}
+
+}
