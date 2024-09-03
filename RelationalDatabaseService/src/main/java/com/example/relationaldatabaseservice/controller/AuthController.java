a901552f4a1512752fa0fb0efe871dd404dd4de8
petrovicmilica <116906072+petrovicmilica@users.noreply.github.com>
2024-09-03 14:15:11 +0200

    fix: Fix login and register

--- RelationalDatabaseService/src/main/java/com/example/relationaldatabaseservice/controller/AuthController.java
+++ RelationalDatabaseService/src/main/java/com/example/relationaldatabaseservice/controller/AuthController.java
@@ -1,37 +1,61 @@
 package com.example.relationaldatabaseservice.controller;
 
+import com.example.relationaldatabaseservice.enums.UserRole;
 import com.example.relationaldatabaseservice.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.relationaldatabaseservice.service.UserService;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestParam String email, @RequestParam String password) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> loginData) {
+        String email = loginData.get("email");
+        String password = loginData.get("password");
+
         String loginResult = userService.loginUser(email, password);
         Map<String, String> response = new HashMap<>();
         if ("User not found".equals(loginResult)) {
-            response.put("error", "User not found");
+            response.put("error", "User with this email address does not exist.");
             return ResponseEntity.status(404).body(response);
         } else if ("Incorrect password".equals(loginResult)) {
-            response.put("error", "Incorrect password");
+            response.put("error", "Incorrect password.");
             return ResponseEntity.status(401).body(response);
         }
         response.put("message", "Login successful");
         return ResponseEntity.ok(response);
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String>> register(@RequestBody User user) {
+        Map<String, String> response = new HashMap<>();
+
+        if (userService.emailExists(user.getEmail())) {
+            response.put("error", "Email already in use");
+            return ResponseEntity.status(409).body(response);
+        }
+
+        if (userService.phoneNumberExists(user.getPhoneNumber())) {
+            response.put("error", "Phone number already in use");
+            return ResponseEntity.status(409).body(response);
+        }
+
+        user.setRole(UserRole.GUEST);
+        user.setPassword(user.getPassword());
+        userService.saveUser(user);
+
+        response.put("message", "User registered successfully");
+        return ResponseEntity.status(201).body(response);
     }
 
 }
--- RelationalDatabaseService/src/main/java/com/example/relationaldatabaseservice/service/UserService.java
+++ RelationalDatabaseService/src/main/java/com/example/relationaldatabaseservice/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.relationaldatabaseservice.service;
 
 import com.example.relationaldatabaseservice.model.User;
 import com.example.relationaldatabaseservice.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,5 +19,22 @@ public class UserService {
             return "Incorrect password";
         }
         return "Success";
+    }
+
+    public boolean emailExists(String email) {
+        return userRepository.findByEmail(email) != null;
+    }
+
+    public boolean phoneNumberExists(String phoneNumber) {
+        return userRepository.findByPhoneNumber(phoneNumber) != null;
+    }
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void rollbackUser(Long userId) {
+        userRepository.deleteById(userId);
     }
 }
